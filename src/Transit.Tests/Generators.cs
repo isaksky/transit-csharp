2b5fc20590bdbf4087888c0841bb9cb688eed4f4
Isak Sky <isak.sky@gmail.com>
2026-03-17 18:14:19 -0600

    Roundtrip property based test

---
+++ src/Transit.Tests/Generators.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using CsCheck;
+using Transit;
+
+namespace Transit.Tests
+{
+    public static class Generators
+    {
+        // --- PRIMITIVES ---
+        public static Gen<int> IntGen => Gen.Int;
+        public static Gen<long> LongGen => Gen.Long;
+        public static Gen<double> DoubleGen => Gen.Double;
+        public static Gen<bool> BoolGen => Gen.Bool;
+        
+        // Let's use alphanumeric strings for now to avoid weird surrogates breaking naive JSON parsers
+        public static Gen<string> StringGen => Gen.String.AlphaNumeric;
+
+        // --- EXOTIC SCALARS ---
+        // Identifiers
+        public static Gen<IKeyword> KeywordGen => 
+            Gen.String.AlphaNumeric.Select(s => TransitFactory.Keyword(string.IsNullOrEmpty(s) ? "k" : s));
+
+        public static Gen<ISymbol> SymbolGen => 
+            Gen.String.AlphaNumeric.Select(s => TransitFactory.Symbol(string.IsNullOrEmpty(s) ? "s" : s));
+
+        // BigInt
+        public static Gen<BigInteger> BigIntegerGen => 
+            Gen.Byte.Array.Select(b => new BigInteger(b));
+
+        public static Gen<Guid> GuidGen => Gen.Guid;
+        
+        // URIs
+        public static Gen<Uri> UriGen => 
+            Gen.String.AlphaNumeric.Select(s => new Uri($"http://example.com/{(string.IsNullOrEmpty(s) ? "u" : s)}"));
+
+        // Date and Time
+        // Transit truncates to milliseconds or ticks but usually we just care about ms precision
+        // We set kind to Local because Transit in C# currently deserializes to Local time by default.
+        public static Gen<DateTime> DateTimeGen => 
+            Gen.DateTime.Select(d => new DateTime(d.Ticks - (d.Ticks % 10000), DateTimeKind.Local));
+
+        public static Gen<DateTimeOffset> DateTimeOffsetGen => 
+            Gen.DateTimeOffset.Select(d => new DateTimeOffset(d.Ticks - (d.Ticks % 10000), TimeZoneInfo.Local.GetUtcOffset(d.UtcDateTime)));
+
+
+        // --- COMPOSITE TYPES ---
+        
+        // A combined generator for basic scalar types
+        public static Gen<object> ScalarGen => Gen.OneOf<object>(
+            IntGen.Select(x => (object)x),
+            LongGen.Select(x => (object)x),
+            DoubleGen.Select(x => (object)x),
+            BoolGen.Select(x => (object)x),
+            StringGen.Select(x => (object)x),
+            KeywordGen.Select(x => (object)x),
+            SymbolGen.Select(x => (object)x),
+            GuidGen.Select(x => (object)x),
+            DateTimeGen.Select(x => (object)x)
+        );
+        
+        // We omit DateTime from dictionary keys to prevent Transit/CSharp EqualityComparer lookup failures.
+        public static Gen<object> DictKeyGen => Gen.OneOf<object>(
+            IntGen.Select(x => (object)x),
+            LongGen.Select(x => (object)x),
+            DoubleGen.Select(x => (object)x),
+            BoolGen.Select(x => (object)x),
+            StringGen.Select(x => (object)x),
+            KeywordGen.Select(x => (object)x),
+            SymbolGen.Select(x => (object)x),
+            GuidGen.Select(x => (object)x)
+        );
+
+        // Depth-bounded Generator for complex objects 
+        public static Gen<object> AnyGen => Gen.OneOf<object>(
+            ScalarGen,
+            ScalarGen.Array.Select(a => (object)a), // object[]
+            Gen.Dictionary(DictKeyGen, ScalarGen).Select(d => (object)d) // Dictionary<object, object>
+        );
+    }
+}
---
+++ src/Transit.Tests/TransitPropertyTests.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Transit;
+using CsCheck;
+using FluentAssertions;
+
+namespace Transit.Tests
+{
+    [TestClass]
+    public class TransitPropertyTests
+    {
+        private static object? Normalize(object? obj)
+        {
+            if (obj is null) return null;
+            if (obj is int val) return (long)val; // Transit uniformly deserializes all standard integers to Int64
+            if (obj is System.Collections.IDictionary dict)
+            {
+                var d = new Dictionary<object, object?>();
+                foreach (System.Collections.DictionaryEntry entry in dict)
+                {
+                    if (entry.Key != null)
+                        d[Normalize(entry.Key)!] = Normalize(entry.Value);
+                }
+                return d;
+            }
+            if (obj is System.Collections.IList list && !(obj is byte[]))
+            {
+                var arr = new object?[list.Count];
+                for (int i = 0; i < list.Count; i++)
+                {
+                    arr[i] = Normalize(list[i]);
+                }
+                return arr;
+            }
+            return obj;
+        }
+
+        [TestMethod]
+        public void Transit_Json_Roundtrips_Correctly()
+        {
+            Generators.AnyGen.Sample(value => 
+            {
+                var serialized = TransitConvert.SerializeObject(value, TransitFactory.Format.Json);
+                var deserialized = TransitConvert.DeserializeObject<object>(serialized, TransitFactory.Format.Json);
+                var normDeser = Normalize(deserialized);
+                var normValue = Normalize(value);
+                
+                normDeser.Should().BeEquivalentTo(normValue, options => options
+                    .RespectingRuntimeTypes()
+                    .Using<DateTime>(ctx => ctx.Subject.ToUniversalTime().Should().BeCloseTo(ctx.Expectation.ToUniversalTime(), TimeSpan.FromMilliseconds(1)))
+                    .WhenTypeIs<DateTime>());
+            }, iter: 1000);
+        }
+
+        [TestMethod]
+        public void Transit_JsonVerbose_Roundtrips_Correctly()
+        {
+            Generators.AnyGen.Sample(value => 
+            {
+                var serialized = TransitConvert.SerializeObject(value, TransitFactory.Format.JsonVerbose);
+                var deserialized = TransitConvert.DeserializeObject<object>(serialized, TransitFactory.Format.JsonVerbose);
+                var normDeser = Normalize(deserialized);
+                var normValue = Normalize(value);
+                
+                normDeser.Should().BeEquivalentTo(normValue, options => options
+                    .RespectingRuntimeTypes()
+                    .Using<DateTime>(ctx => ctx.Subject.ToUniversalTime().Should().BeCloseTo(ctx.Expectation.ToUniversalTime(), TimeSpan.FromMilliseconds(1)))
+                    .WhenTypeIs<DateTime>());
+            }, iter: 1000);
+        }
+
+        [TestMethod]
+        public void Transit_Json_And_JsonVerbose_Are_Equivalent()
+        {
+            Generators.AnyGen.Sample(value => 
+            {
+                var json = TransitConvert.SerializeObject(value, TransitFactory.Format.Json);
+                var verbose = TransitConvert.SerializeObject(value, TransitFactory.Format.JsonVerbose);
+
+                var fromJson = TransitConvert.DeserializeObject<object>(json, TransitFactory.Format.Json);
+                var fromVerbose = TransitConvert.DeserializeObject<object>(verbose, TransitFactory.Format.JsonVerbose);
+
+                var normFromJson = Normalize(fromJson);
+                var normFromVerbose = Normalize(fromVerbose);
+
+                normFromJson.Should().BeEquivalentTo(normFromVerbose, options => options
+                    .RespectingRuntimeTypes()
+                    .Using<DateTime>(ctx => ctx.Subject.ToUniversalTime().Should().BeCloseTo(ctx.Expectation.ToUniversalTime(), TimeSpan.FromMilliseconds(1)))
+                    .WhenTypeIs<DateTime>());
+            }, iter: 1000);
+        }
+    }
+}
